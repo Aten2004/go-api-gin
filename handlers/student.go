d55f2bf776374d0cbfba77fa6517215b3f960ae2
PHURITfuengfu <phurit.fue@dome.tu.ac.th>
2026-02-07 23:23:28 +0700

    Update project structure to layered architecture for bonus points

---
+++ handlers/student.go
@@ -0,0 +1,62 @@
+package handlers
+
+import (
+	"github.com/Aten2004/go-api-gin/models" 
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+	"net/http"
+)
+
+// สร้างตัวแปรสำหรับรับ Database connection จาก main
+var db *gorm.DB
+
+func SetDB(database *gorm.DB) {
+	db = database
+}
+
+func GetStudents(c *gin.Context) {
+	var students []models.Student
+	db.Find(&students)
+	c.JSON(http.StatusOK, students)
+}
+
+func CreateStudent(c *gin.Context) {
+	var student models.Student
+	if err := c.ShouldBindJSON(&student); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	db.Create(&student)
+	c.JSON(http.StatusCreated, student)
+}
+
+func UpdateStudent(c *gin.Context) {
+	id := c.Param("id")
+	var student models.Student
+
+	if err := db.First(&student, "id = ?", id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Student not found"})
+		return
+	}
+
+	if err := c.ShouldBindJSON(&student); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	db.Save(&student)
+	c.JSON(http.StatusOK, student)
+}
+
+func DeleteStudent(c *gin.Context) {
+	id := c.Param("id")
+	var student models.Student
+
+	if err := db.First(&student, "id = ?", id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Student not found"})
+		return
+	}
+
+	db.Delete(&student)
+	c.Status(http.StatusNoContent)
+}
